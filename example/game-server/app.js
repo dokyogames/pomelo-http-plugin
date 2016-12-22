ab060fd08b05f5746397f431062eb694000a8aa8
yi.luo <yi.luo@silverera.net>
2016-12-22 18:07:26 +0800

    update express

--- example/game-server/app.js
+++ example/game-server/app.js
@@ -11,23 +11,23 @@ app.set('name', 'example');
 
 
 // app configuration
-app.configure('development', 'gamehttp', function() {
-	app.loadConfig('httpConfig', path.join(app.getBase(), 'config/http.json'));
-	app.use(httpPlugin, {
-		http: app.get('httpConfig').gamehttp,
-	});
-	app.use(httpPlugin, {
-		http: app.get('httpConfig').gamehttps,
-	});
+app.configure('development', 'gamehttp', function () {
+    app.loadConfig('httpConfig', path.join(app.getBase(), 'config/http.json'));
+    app.use(httpPlugin, {
+        http: app.get('httpConfig').gamehttp,
+    });
+    app.use(httpPlugin, {
+        http: app.get('httpConfig').gamehttps,
+    });
 
-	httpPlugin.filter(require('./app/filters/log')());
-	httpPlugin.afterFilter(function(req, res) {
-		res.send(res.get('resp'));
-	});
+    httpPlugin.filter(require('./app/filters/log')());
+    httpPlugin.afterFilter(function (req, res) {
+        res.send(res.get('resp'));
+    });
 });
 // start app
 app.start();
 
-process.on('uncaughtException', function(err) {
-	console.error(' Caught exception: ' + err.stack);
+process.on('uncaughtException', function (err) {
+    console.error(' Caught exception: ' + err.stack);
 });
--- example/game-server/app/servers/gamehttp/route/testRoute.js
+++ example/game-server/app/servers/gamehttp/route/testRoute.js
@@ -1,20 +1,18 @@
 'use strict';
 
-module.exports = function(app, http, plugin) {
+module.exports = function (app, http, plugin) {
+    if (plugin.useSSL) {
+        http.get('/testHttps', function (req, res, next) {
+            // console.log(req.body);
+            res.set('resp', 'https success');
+            next();
+        });
+    } else {
 
-	if (plugin.useSSL) {
-
-		http.get('/testHttps', function(req, res, next) {
-			// console.log(req.body);
-			res.set('resp', 'https success');
-			next();
-		});
-	} else {
-
-		http.get('/testHttp', function(req, res, next) {
-			// console.log(req.body);
-			res.set('resp', 'http success');
-			next();
-		});
-	}
+        http.get('/testHttp', function (req, res, next) {
+            // console.log(req.body);
+            res.set('resp', 'http success');
+            next();
+        });
+    }
 };
